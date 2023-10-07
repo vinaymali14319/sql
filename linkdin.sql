2335d7439be62822f6360a7c9e5b6e00a8ff456f
vinaymali14319 <77825476+vinaymali14319@users.noreply.github.com>
2023-10-07 17:06:54 +0530

    Update linkdin.sql

--- linkdin.sql
+++ linkdin.sql
@@ -23,5 +23,28 @@ select * from Products;
 
 select sell_date , count(product) , group_concat(product) from  Products  group by sell_date;
 
+=========================================================================================================================================
+
+create table brands
+(
+ Year int,
+ Brand varchar(20),
+ Amount int
+);
+insert into brands values (2018, 'Apple', 45000);
+insert into brands values (2019, 'Apple', 35000);
+insert into brands values (2020, 'Apple', 75000);
+insert into brands values (2018, 'Samsung', 15000);
+insert into brands values (2019, 'Samsung', 20000);
+insert into brands values (2020, 'Samsung', 25000);
+insert into brands values (2018, 'Nokia', 21000);
+insert into brands values (2019, 'Nokia', 17000);
+insert into brands values (2020, 'Nokia', 14000);
+
+select * from brands;
+select distinct(b.brand) from brands b where brand not in (
+select a.brand from (
+select   brand , Amount,Amount-lag (Amount ,1 ) over (PARTITION BY brand ORDER BY year ) as amt  from brands) a where a.amt < 0);
+
 
 
